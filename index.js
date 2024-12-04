36e05693fb774b8fa129a5263b10a6d1ce3ad956
Merlijn <44466724+MBrinks2239@users.noreply.github.com>
2024-12-04 13:54:53 +0100

    Dynamic switches

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const { app, BrowserWindow } = require("electron");
 
-var profileName = app.commandLine.getSwitchValue("profile");
+let profileName = app.commandLine.getSwitchValue("profile");
+let shouldNotMaximize = app.commandLine.getSwitchValue("no-maximize");
+let appHeight = parseInt(app.commandLine.getSwitchValue("height"));
+let appWidth = parseInt(app.commandLine.getSwitchValue("width"));
+
+console.log("Profile:", profileName);
+console.log("No Maximize:", shouldNotMaximize);
+console.log("Height:", appHeight);
+console.log("Width:", appWidth);
+
 if (profileName === "") {
   profileName = process.argv[2];
 }
@@ -10,22 +19,32 @@ if (profileName === "" || profileName == undefined) {
 }
 
 const createWindow = () => {
+  if (isNaN(appHeight) || appHeight == undefined) {
+    appHeight = 1080;
+  }
+  if (isNaN(appWidth) || appWidth == undefined) {
+    appWidth = 1920;
+  }
+
   const win = new BrowserWindow({
     autoHideMenuBar: true,
     show: false,
-    width: 1920,
-    height: 1080,
+    width: appWidth,
+    height: appHeight,
     webPreferences: {
       partition: profileName ? `persist:${profileName}` : undefined,
     },
   });
 
-  win.maximize();
+  if (shouldNotMaximize !== "true") {
+    win.maximize();
+  }
 
   win.loadURL("https://universe.flyff.com/play");
 
   win.once("ready-to-show", () => {
     win.setTitle(`Flyff Universe - ${profileName}`);
+    win.setSize(appWidth, appHeight);
     win.show();
 
     win.webContents.setZoomFactor(1);
